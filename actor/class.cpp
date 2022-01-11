87011b974120f845eb38410359ca514bb56b2399
Nick Codignotto <nick.codignotto@gmail.com>
2022-01-11 14:27:18 +0000

    New program

---
+++ actor/class.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "caf/actor_system.hpp"
+#include "caf/caf_main.hpp"
+#include "caf/event_based_actor.hpp"
+
+using namespace caf;
+
+struct Foo {
+  int bar;
+
+  Foo() { bar = 0; }
+
+  Foo(const Foo& foo) { bar = foo.bar }
+
+  template <class Inspector> bool inspect(Inspector& f, Foo& x) { return f.object(x).fields(f.field("bar", x.bar)); };
+}
+
+behavior
+mirror(event_based_actor* self) {
+
+  return {
+    [=](const Foo& foo) { std::cout << foo.bar << std::endl; },
+  };
+}
+
+void begin(event_based_actor* self, const actor& other) {
+  Foo foo{ 1 };
+  self->request(other, std::chrono::seconds(10), foo);
+}
+
+void caf_main(actor_system& sys) {
+  auto mirror_actor = sys.spawn(mirror);
+  sys.spawn(mirror);
+}
+
+CAF_MAIN()
