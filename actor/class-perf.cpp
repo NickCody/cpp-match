90264f7de2b59ab162168595257a768992a6bd15
Nick Codignotto <nick.codignotto@gmail.com>
2022-01-16 05:57:16 +0000

    csv output

--- actor/class-perf.cpp
+++ actor/class-perf.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <mutex>
 #include <queue>
+#include <memory>
 #include <cstdlib>
 #include "include/fmt/format.h"
 
@@ -68,65 +69,69 @@ void busy_spin(std::chrono::microseconds spin_time) {
   }
 }
 
+void print_queue_analysis(uint64_t num_threads) {
+  std::vector<uint64_t> queue_analysis;
+  queue_analysis.reserve(num_threads);
+  for (uint64_t i = 0; i < num_threads; i++) {
+    queue_analysis[i] = 0;
+  }
+  while (!g_count_queue.empty()) {
+    queue_analysis[g_count_queue.front()]++;
+    g_count_queue.pop();
+  }
+
+  for (uint64_t i = 0; i < num_threads; i++) {
+    fmt::print("queue_analysis[{}] = {}\n", i, queue_analysis[i]);
+  }
+}
+
+void print_counts() {
+  fmt::print(" {:>30} = {:>15}\n", "g_par_work_count", common::numberFormatWithCommas(g_par_work_count));
+  fmt::print(" {:>30} = {:>15}\n", "g_par_send_count", common::numberFormatWithCommas(g_par_send_count));
+
+  fmt::print(" {:>30} = {:>15}\n", "g_actor_pool_work_count", common::numberFormatWithCommas(g_actor_pool_work_count));
+  fmt::print(" {:>30} = {:>15}\n", "g_actor_pool_send_count", common::numberFormatWithCommas(g_actor_pool_send_count));
+}
+
 // =-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 // Actor setup
 //
-struct Foo;
-struct Bar;
-
-CAF_BEGIN_TYPE_ID_BLOCK(custom_types_1, first_custom_type_id)
-CAF_ADD_TYPE_ID(custom_types_1, (Foo))
-CAF_ADD_TYPE_ID(custom_types_1, (Bar))
-CAF_END_TYPE_ID_BLOCK(custom_types_1)
 
 struct Foo {
-  int bar;
+  uint64_t bar;
 };
 
-struct Bar {
-  int foo;
+struct CounterData {
+  std::shared_ptr<uint64_t> count;
 };
+
+CAF_BEGIN_TYPE_ID_BLOCK(class_perf, first_custom_type_id)
+
+CAF_ADD_TYPE_ID(class_perf, (Foo))
+
+CAF_ADD_ATOM(class_perf, work)
+CAF_ADD_ATOM(class_perf, report)
+
+CAF_END_TYPE_ID_BLOCK(class_perf)
 
 template <class Inspector> bool inspect(Inspector& f, Foo& x) {
   return f.object(x).fields(f.field("bar", x.bar));
 }
 
-template <class Inspector> bool inspect(Inspector& f, Bar& x) {
-  return f.object(x).fields(f.field("foo", x.foo));
-}
-
-behavior posh(event_based_actor* self, const actor& count_actor) {
+behavior posh_pool(stateful_actor<Foo>* self, const actor& count_actor) {
   return {
-    [=](const Foo& val) {
-      self->send(count_actor, val);
+    [=](work) {
+      self->send(count_actor, work_v);
       busy_spin(g_busy_spin_millis);
     },
   };
 }
 
-behavior posh_x(event_based_actor* self, const actor& count_actor) {
+behavior posh_counter(stateful_actor<CounterData>* self) {
+  self->state.count = std::make_shared<uint64_t>(0);
   return {
-    [=](const Foo& val) {
-      self->send(count_actor, val);
-      busy_spin(g_busy_spin_millis);
-    },
-  };
-}
-
-behavior posh_pool(event_based_actor* self, const actor& count_actor) {
-  return {
-    [=](const Foo& val) {
-      self->send(count_actor, val);
-      busy_spin(g_busy_spin_millis);
-    },
-  };
-}
-
-behavior posh_counter(event_based_actor* /*self*/) {
-  auto count = std::make_shared<uint64_t>(0);
-  return {
-    [=](const Foo& /*val*/) { (*count)++; },
-    [=](const Bar& /*val*/) -> uint64_t { return *count; },
+    [=](work) { (*self->state.count)++; },
+    [=](report) -> uint64_t { return *self->state.count; },
   };
 }
 
@@ -150,80 +155,12 @@ void par_func(int max_to_read, uint64_t id) {
   }
 }
 
-// -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
-// time_actor
-// -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
-
-void time_actor(actor_system& sys, const WorkConfig& work_config) {
-  common::Stopwatch<uint64_t, std::chrono::microseconds> watch;
-  g_actor_send_count = 0;
-  g_actor_work_count = 0;
-  while (!g_count_queue.empty())
-    g_count_queue.pop();
-
-  double micros = watch.duration([&] {
-    Foo f{ 1 };
-    auto counter = sys.spawn(posh_counter);
-    auto posh_actor = sys.spawn(posh, counter);
-    scoped_actor self{ sys };
-
-    for (uint64_t i = 0; i < work_config.iterations; i++) {
-      self->send(posh_actor, f);
-      g_actor_send_count++;
-      std::this_thread::sleep_for(work_config.work_send_delay);
-    }
-    self->send(posh_actor, exit_msg{ self.address(), exit_reason::kill });
-    self->await_all_other_actors_done();
-  });
-
-  fmt::print("{:>25}: {:>15} (micros)\n", "single actor", common::numberFormatWithCommas(micros));
-
-  g_actor_work_count = g_count_queue.size();
-}
-
-// -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
-// time_actor_x
-// -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
-
-void time_actor_x(actor_system& sys, const WorkConfig& work_config) {
-  common::Stopwatch<uint64_t, std::chrono::microseconds> watch;
-  g_actor_x_send_count = 0;
-  g_actor_x_work_count = 0;
-  scoped_actor self{ sys };
-  auto counter = sys.spawn(posh_counter);
-
-  while (!g_count_queue.empty())
-    g_count_queue.pop();
-
-  double micros = watch.duration([&] {
-    Foo f{ 1 };
-
-    for (uint64_t i = 0; i < work_config.iterations; i++) {
-      auto posh_actor = sys.spawn(posh_x, counter);
-      self->send(posh_actor, f);
-      g_actor_x_send_count++;
-      self->send(posh_actor, exit_msg{ self.address(), exit_reason::kill });
-      std::this_thread::sleep_for(work_config.work_send_delay);
-    }
-    self->await_all_other_actors_done();
-  });
-
-  fmt::print("{:>25}: {:>15} (micros)\n", "actor per work item", common::numberFormatWithCommas(micros));
-
-  g_actor_work_count = g_count_queue.size();
-}
-
-// -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
-// time_actor_x
-// -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
-
-void time_actor_pool(actor_system& sys, const WorkConfig& work_config) {
+std::chrono::microseconds time_actor_pool(actor_system& sys, const WorkConfig& work_config) {
   common::Stopwatch<uint64_t, std::chrono::microseconds> watch;
   g_actor_pool_send_count = 0;
   g_actor_pool_work_count = 0;
 
   double micros = watch.duration([&] {
-    Foo f{ 1 };
     scoped_actor self{ sys };
     auto counter = sys.spawn(posh_counter);
     scoped_execution_unit ctx{ &sys };
@@ -232,12 +169,12 @@ void time_actor_pool(actor_system& sys, const WorkConfig& work_config) {
 
     for (uint64_t i = 0; i < work_config.iterations; i++) {
       g_actor_pool_send_count++;
-      self->send(pool, f);
+      self->send(pool, work_v);
       std::this_thread::sleep_for(work_config.work_send_delay);
     }
 
     while (g_actor_pool_work_count < g_actor_pool_send_count) {
-      self->request(counter, infinite, Bar{ 1 })
+      self->request(counter, infinite, report_v)
           .receive([=](uint64_t c) { g_actor_pool_work_count = c; }, [&](error& err) { fmt::print("Error: {}\n", to_string(err)); });
     }
 
@@ -246,45 +183,16 @@ void time_actor_pool(actor_system& sys, const WorkConfig& work_config) {
     self->await_all_other_actors_done();
   });
 
-  fmt::print("{:>25}: {:>15} (micros)\n", "actor pool", common::numberFormatWithCommas(micros));
-
   sys.await_all_actors_done();
-}
 
-// -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
-// time_func
-// -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
-
-void time_func(actor_system& /*sys*/, const WorkConfig& work_config) {
-  common::Stopwatch<uint64_t, std::chrono::microseconds> watch;
-  g_func_send_count = 0;
-  g_func_work_count = 0;
-
-  while (!g_count_queue.empty())
-    g_count_queue.pop();
-
-  auto func = [&](int inc) {
-    g_func_send_count += inc;
-    busy_spin(g_busy_spin_millis);
-  };
-
-  double micros = watch.duration([&] {
-    for (uint64_t i = 0; i < work_config.iterations; i++) {
-      func(1);
-      std::this_thread::sleep_for(work_config.work_send_delay);
-    }
-  });
-
-  fmt::print("{:>25}: {:>15} (micros)\n", "raw function", common::numberFormatWithCommas(micros));
-
-  g_actor_work_count = g_count_queue.size();
+  return std::chrono::microseconds((uint64_t)micros);
 }
 
 // -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
 // time_par_func
 // -==------=-==-=-=-===-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-===-=-=-==
 
-void time_par_func(actor_system& /*sys*/, const WorkConfig& work_config) {
+std::chrono::microseconds time_par_func(const WorkConfig& work_config) {
   common::Stopwatch<uint64_t, std::chrono::microseconds> watch;
   g_par_continue.store(true);
   int num_to_read = work_config.par_threads == 1 ? -1 : 1;
@@ -298,7 +206,6 @@ void time_par_func(actor_system& /*sys*/, const WorkConfig& work_config) {
     g_count_queue.pop();
 
   double micros = watch.duration([&] {
-    
     // prime queue
     for (uint64_t i = 0; i < work_config.iterations; i++) {
       g_queue.push(g_par_send_count);
@@ -320,65 +227,30 @@ void time_par_func(actor_system& /*sys*/, const WorkConfig& work_config) {
     }
   });
 
-  fmt::print("{:>25}: {:>15} (micros)\n", "standard thread pool", common::numberFormatWithCommas(micros));
-
   g_par_work_count = g_count_queue.size();
+
+  return std::chrono::microseconds((uint64_t)micros);
 }
 
-void print_queue_analysis(uint64_t num_threads) {
-  std::vector<uint64_t> queue_analysis;
-  queue_analysis.reserve(num_threads);
-  for (uint64_t i = 0; i < num_threads; i++) {
-    queue_analysis[i] = 0;
-  }
-  while (!g_count_queue.empty()) {
-    queue_analysis[g_count_queue.front()]++;
-    g_count_queue.pop();
-  }
-
-  for (uint64_t i = 0; i < num_threads; i++) {
-    fmt::print("queue_analysis[{}] = {}\n", i, queue_analysis[i]);
-  }
+void print_config_row(const WorkConfig& work_config) {
+  fmt::print("{},{},{},{},{},{}",
+             work_config.par_threads,
+             work_config.actor_threads,
+             work_config.actor_pool_size,
+             work_config.iterations,
+             g_busy_spin_millis.count(),
+             work_config.random_work);
 }
 
-void print_counts() {
-  // fmt::print(" {:>30} = {:>15}\n", "g_func_work_count", common::numberFormatWithCommas(g_func_work_count));
-  // fmt::print(" {:>30} = {:>15}\n", "g_func_send_count", common::numberFormatWithCommas(g_func_send_count));
+void harness(actor_system& sys, const WorkConfig& work_config) {
 
-  fmt::print(" {:>30} = {:>15}\n", "g_par_work_count", common::numberFormatWithCommas(g_par_work_count));
-  fmt::print(" {:>30} = {:>15}\n", "g_par_send_count", common::numberFormatWithCommas(g_par_send_count));
+  std::chrono::microseconds par_time = time_par_func(g_work_config);
+  std::chrono::microseconds actor_time = time_actor_pool(sys, g_work_config);
 
-  // fmt::print(" {:>30} = {:15}\n", "g_actor_work_count", common::numberFormatWithCommas(g_actor_work_count));
-  // fmt::print(" {:>30} = {:>15}\n", "g_actor_send_count", common::numberFormatWithCommas(g_actor_send_count));
-
-  // fmt::print(" {:>30} = {:>15}\n", "g_actor_x_work_count", common::numberFormatWithCommas(g_actor_x_work_count));
-  // fmt::print(" {:>30} = {:>15}\n", "g_actor_x_send_count", common::numberFormatWithCommas(g_actor_x_send_count));
-
-  fmt::print(" {:>30} = {:>15}\n", "g_actor_pool_work_count", common::numberFormatWithCommas(g_actor_pool_work_count));
-  fmt::print(" {:>30} = {:>15}\n", "g_actor_pool_send_count", common::numberFormatWithCommas(g_actor_pool_send_count));
-}
-
-void harness(actor_system& sys) {
-
-  fmt::print("Using {} par_threads, {} actor_threads, {} actor_pool_size, {} iterations, with {} microsecond work per "
-             "iteration, random_work={}:\n\n",
-             g_work_config.par_threads,
-             g_work_config.actor_threads,
-             common::numberFormatWithCommas(g_work_config.actor_pool_size),
-             common::numberFormatWithCommas(g_work_config.iterations),
-             common::numberFormatWithCommas(g_busy_spin_millis.count()),
-             g_work_config.random_work);
-  sys.clock();
-
-  // time_func(sys, g_work_config.iterations,g_work_config.work_send_delay);
-  time_par_func(sys, g_work_config);
-  // time_actor(sys, g_work_config);
-  // time_actor_x(sys, g_work_config);
-  time_actor_pool(sys, g_work_config);
-
-  // print_counts();
-
-  fmt::print("\n");
+  print_config_row(work_config);
+  fmt::print(",{},{}\n", "parallel function", par_time.count());
+  print_config_row(work_config);
+  fmt::print(",{},{}\n", "actor pool", actor_time.count());
 }
 
 void caf_main(actor_system& sys) {
@@ -393,14 +265,16 @@ void caf_main(actor_system& sys) {
     get_or(sys.config(), "class-perf.random-work", false),
   };
 
+  fmt::print("{},{},{},{},{},{},{},{}\n", "par_threads", "actor_threads", "pool_size", "iterations", "busy_spin", "random", "method", "result");
+
   for (auto work : g_work_config.work_per_iteration) {
     g_busy_spin_millis = std::chrono::duration_cast<std::chrono::microseconds>(work);
     // for (auto n : std::list<size_t>{ 1, 2, 4, 8 }) {
     //   config.par_threads = n;
     //   harness(sys, config);
     // }
-    harness(sys);
+    harness(sys, g_work_config);
   }
 }
 
-CAF_MAIN(id_block::custom_types_1)
+CAF_MAIN(id_block::class_perf)
